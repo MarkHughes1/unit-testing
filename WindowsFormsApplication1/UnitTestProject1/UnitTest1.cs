485509ed4037fdae5533dbf671bb5806ceb676bb
MarkHughes1 <Mark.Hughes4096@gmail.com>
2015-08-25 10:29:41 +0100

    Adding stuff

---
+++ WindowsFormsApplication1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using myCode;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        public void checkadd()
+        {
+         
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/code.cs
@@ -0,0 +1,15 @@
+﻿
+namespace myCode
+{
+    public class code 
+    {
+        public double Add(double a , double b)
+        {
+            var result = 0.0;
+
+            result = a + b;
+
+            return result;
+        }
+    }
+}
